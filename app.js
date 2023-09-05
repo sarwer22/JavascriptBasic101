30156c705a3d071c7e090b8ff6d70d00a1df8ec6
Sarwer <mdsarwer48@gmail.com>
2023-09-05 19:24:11 +0600

    Initial commit done

---
+++ app.js
@@ -0,0 +1,30 @@
+// Task 1: Hello World with Variables
+
+console.log("Hello World");
+
+var greeting = "Hello World";
+console.log(greeting);
+console.log(typeof greeting);
+
+var introduceMyself = "Hallow this is Sarwer, from Dhaka.";
+console.log(introduceMyself);
+console.log(typeof introduceMyself);
+
+var myName = "Md Golam sarwer";
+console.log(myName);
+console.log(typeof myName);
+
+// Task: 2 Basic Math Operations
+
+var num1 = 20;
+var num2 = 30;
+var total = num1 + num2;
+console.log(num1 + num2);
+console.log(total);
+console.log(typeof num1);
+console.log(typeof total);
+
+console.log(num2 - num1);
+console.log(num2 * num1);
+console.log(num2 / num1);
+console.log(num2 % num1);
